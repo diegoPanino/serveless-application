5b3c4dded3aa017d21b864427b6e0a794713c29c
diegoPanino <tieso.diego@gmail.com>
2022-04-20 13:55:48 +0200

    Wrote function

--- late.js
+++ late.js
@@ -1,1 +1,13 @@
-// start coding your function here!
+function running_late(date){
+    const year = date.getYear();
+    const month = date.getYear();
+    const day = date.getDate();
+
+    const compareDate = new Date(year,month,day,22,00,00);
+    if(date >= compareDate)
+        return 'It is late!';
+    else
+        return 'It is still early';
+}
+
+module.exports = running_late;
