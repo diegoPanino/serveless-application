b03125c4d78cf07b233ccf77f483b557ebbfe58f
diegoPanino <tieso.diego@gmail.com>
2022-04-20 14:22:29 +0200

    Running late function

--- late.js
+++ late.js
@@ -1,16 +1,19 @@
 function running_late(date){
-    const year = date.getYear();
-    const month = date.getYear();
-    const day = date.getDate();
-
-    const compareDate = new Date(year,month,day,22,00,00);
+    const inputDate = new Date(date)
+    const compareDate = new Date(date)
+    
+    compareDate.setHours(22,00,00);
+    
     let result;
-    if(date >= compareDate)
+    
+    if(inputDate >= compareDate)
         result = 'It is late!';
     else
-        result = 'It is still early';
+        result = 'It is still early!';
     
     return result;
 }
-
-module.exports = running_late;
+//exports.running_late = running_late;
+module.exports = {
+    running_late:running_late
+}
