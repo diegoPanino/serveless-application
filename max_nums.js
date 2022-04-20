5871823b3f3fae0eb5c519a7affc0e40432bcba0
diegoPanino <tieso.diego@gmail.com>
2022-04-20 14:40:36 +0200

    Wrote max function

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,13 @@
-// start coding your function here!
+function find_max(arr){
+    let max = 0;
+    for(let i = 0; i < arr.length -1 ; i ++){
+        if(arr[i] > max)
+            max = arr[i]
+    }
+    return max;
+    //return Math.max(...arr);
+};
+
+module.exports = {
+    find_max:find_max
+}
